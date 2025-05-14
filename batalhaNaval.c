3e3b8ecfb2b33877eccfbddd293872e0fbeb5c4b
Denis Mendonça <98358197+denishmo@users.noreply.github.com>
2025-05-14 14:31:55 +0000

    batalha naval aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,26 +5,35 @@ int main() {
 
     int tabuleiro[10][10] = {0}; // Matriz 10x10 inicializada com zeros
 
-    // Linha 2, coluna B
-    for (int j = 1; j < 1 + 3; j++){
-    tabuleiro[1][j] = 3; 
+    // Horizontal
+    for (int j = 0; j < 0 + 3; j++){
+    tabuleiro[4][j] = 3; 
     }
 
-    // Linnha 8, coluna J
+    // Vertical
     for (int i = 7; i < 7 + 3; i++){
         tabuleiro[i][9] = 3; 
     }
 
-    // Imprimir cabeçalho com letras A a J (colunas)
+    // Diagonal direita
+    for (int k = 0; k < 3; k++) {
+    tabuleiro[0 + k][0 + k] = 3;
+    }
+
+    // Diagonal esquerda
+    for (int l = 0; l < 3; l++) {
+    tabuleiro[7 + l][2 - l] = 3;
+}
+    // Aqui ele imprimi o cabeçalho com letras A a J (colunas)
     printf("   "); // Espaço para o índice da linha
     for (char coluna = 'A'; coluna <= 'J'; coluna++) {
         printf(" %c", coluna);
     }
     printf("\n");
 
-    // Imprimir linhas com rótulo numérico de 1 a 10
+    // Aqui ele imprimi linhas numéricas de 1 a 10
     for (int i = 0; i < 10; i++) {
-        printf("%2d ", i + 1); // Rótulo da linha (1 a 10)
+        printf("%2d ", i + 1); 
         for (int j = 0; j < 10; j++) {
             printf(" %d", tabuleiro[i][j]);
         }
